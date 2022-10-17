5d34d1fef10ffe4a4e2539c7c20cd131070e9d26
FelipeAmorim720 <felipe.reis@bandtec.com.br>
2022-10-17 19:13:56 -0300

    [style] adicionando comentarios para melhor entendimento.

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -13,46 +13,46 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    fun getCustomer(@RequestParam name: String?): List<CustomerModel> {
+    @ResponseStatus(HttpStatus.OK) // 200
+    fun getCustomer(@RequestParam name: String?): List<CustomerModel> { // consultando via requestParam
         name?.let {
-            return customers.filter { it.name.contains(name, true) }
+            return customers.filter { it.name.contains(name, true) } // filtrando se o nome contem name e permitindo ignoreCase(Ignora letras maisculas)
         }
         return customers
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) { // criando usuario.
 
-        var id = if (customers.isEmpty()) {
+        var id = if (customers.isEmpty()) { // if para validar id.
             1
         } else {
-            customers.last().id.toInt() + 1
+            customers.last().id.toInt() + 1 // pegando ultima info da lista e somando +1.
         }.toString()
 
-        customers.add(CustomerModel(id, customer.name, customer.email))
+        customers.add(CustomerModel(id, customer.name, customer.email)) // adicionando na lista com as infos.
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.OK) // 200
     fun getCustomersId(@PathVariable id: String): CustomerModel {
-        return customers.filter { it.id == id }.first()
+        return customers.filter { it.id == id }.first() // pegando a primeira info com o id passado.
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     fun updateCustomer(@PathVariable id: String, @RequestBody customer: PutCustomerRequest){
-        customers.filter { it.id == id }.first().let {
+        customers.filter { it.id == id }.first().let { // pegando a primeira info com o id passado.
             it.name =  customer.name
             it.email = customer.email
         }
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     fun deleteCustomer(@PathVariable id: String){
-        customers.removeIf { it.id == id }
+        customers.removeIf { it.id == id } // removendo a primeira info com o id passado, se existir.
     }
 
 
